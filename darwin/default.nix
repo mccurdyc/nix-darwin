da5df6326d8f0804619df64addc9d4646440f275
Colton McCurdy <mccurdyc@faamac.buffalo-boa.ts.net>
2023-09-24 17:37:06 -0400

    zsh nice
    Signed-off-by: Colton McCurdy <mccurdyc@faamac.buffalo-boa.ts.net>

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,11 +1,3 @@
-#
-#  These are the different profiles that can be used when building on MacOS
-#
-#  flake.nix
-#   └─ ./darwin
-#       ├─ default.nix *
-#       └─ <host>.nix
-#
 {
   lib,
   inputs,
